1e28938d69f4fe0504deb1de251e9ce9db6919d0
tanshi nayak <tanshitn@gmail.com>
2020-08-19 21:54:13 +0530

    Updates

---
+++ precache-manifest.5bd40383128ee07aaa902dc4ba93b28e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "970a43c1266a35d12b929011ec53739b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f6be85334cae43f921a",
+    "url": "/static/css/main.c8d32582.chunk.css"
+  },
+  {
+    "revision": "71f603df525a1411d923",
+    "url": "/static/js/2.be14ecc6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.be14ecc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f6be85334cae43f921a",
+    "url": "/static/js/main.e568ce65.chunk.js"
+  },
+  {
+    "revision": "b7cafb06666c3050b128",
+    "url": "/static/js/runtime-main.9488a020.js"
+  }
+]);
